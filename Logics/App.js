62d7e5f01303b33f913a58262461326c8e82a1a5
Homen Nath <homennath7638@gmail.com>
2023-03-26 21:29:25 +0530

    Modal in Javascript

--- Logics/App.js
+++ Logics/App.js
@@ -2,32 +2,13 @@
 
 
 
-function showInfo() {
-    let res = document.getElementById('res');
-    res.innerHTML = "Hello My name is Homen nath";
-    res.style.color = 'red';
-    res.style.fontWeight = "700";
-}
-function showBack(){
-    let res = document.getElementById('res');
-    res.style.backgroundColor = "yellow";
-}
-const bgChanger = () =>{
-    let bdy = document.getElementById('body');
-    
-    let possible = "ABCDEF0123456789";
-    let bgColor = "";
-    for(let i = 0; i<6; i++) {
-        bgColor += possible.charAt(Math.floor(Math.random() * possible.length))
-    }
-    bdy.style.backgroundColor = "#"+bgColor;
-    document.getElementById('colorCode').innerText = "#"+bgColor;
-    
-}
-const copyCode =()=>{
-    var text = document.getElementById("colorCode").innerHTML;
-    
-    navigator.clipboard.writeText(text)
-    alert(`Copied ${text}`);
 
+
+const OpenModal = ()=>{
+    document.getElementById('modal').style.display = 'block';
+    // modal.style.display = 'block';
+    // console.log('Button Clicked');
+}
+const closeBtn = ()=>{
+    document.getElementById('modal').style.display = 'none';
 }
